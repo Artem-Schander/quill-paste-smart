f6c69274f1a1cbe695f6f867b05ec15455390e53
Artem Schander <schander.artem@gmail.com>
2022-02-26 13:07:49 +0100

    fixes #17 #22 #23

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,41 @@
 const path = require('path');
 
 const config = {
-    entry: './src/module.js',
-    output: {
-        filename: 'quill-paste-smart.js',
-        path: path.resolve(__dirname, 'dist'),
-        library: 'QuillPasteSmart',
-        libraryTarget: 'umd',
+  entry: './src/module.js',
+  output: {
+    filename: 'quill-paste-smart.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'QuillPasteSmart',
+    libraryTarget: 'umd',
+  },
+  optimization: {
+    minimize: true,
+  },
+  target: 'web',
+  mode: 'production',
+  externals: {
+    quill: {
+      commonjs: 'quill',
+      commonjs2: 'quill',
+      amd: 'quill',
+      root: 'Quill',
     },
-    optimization: {
-        minimize: true,
-    },
-    target: 'web',
-    mode: 'production',
-    externals: {
-        quill: {
-            commonjs: 'quill',
-            commonjs2: 'quill',
-            amd: 'quill',
-            root: 'Quill',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [path.resolve(__dirname, 'src/')],
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { modules: false }]],
+          },
         },
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                include: [path.resolve(__dirname, 'src/')],
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [['@babel/preset-env', { modules: false }]],
-                    },
-                },
-            },
-        ],
-    },
+      },
+    ],
+  },
 };
 
 module.exports = config;
