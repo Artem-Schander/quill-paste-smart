6a3f92da4ba90ac4f83812105c95316103e7e90f
Tomáš Stankovič <info@slinto.sk>
2023-07-18 11:19:13 +0200

    run prettier on code

--- src/module.js
+++ src/module.js
@@ -1,8 +1,8 @@
-import Quill from 'quill';
-import DOMPurify from 'dompurify';
+import Quill from "quill";
+import DOMPurify from "dompurify";
 
-const Clipboard = Quill.import('modules/clipboard');
-const Delta = Quill.import('delta');
+const Clipboard = Quill.import("modules/clipboard");
+const Delta = Quill.import("delta");
 
 class QuillPasteSmart extends Clipboard {
   constructor(quill, options) {
@@ -26,9 +26,23 @@ class QuillPasteSmart extends Clipboard {
     const range = this.quill.getSelection();
     if (range == null)  return;
 
-    let text = e.clipboardData?.getData('text/plain');
-    let html = e.clipboardData?.getData('text/html');
-    let file = e.clipboardData?.items?.[0];
+    let text;
+    let html;
+    let file;
+
+    if (
+      (!e.clipboardData || !e.clipboardData.getData) &&
+      window.clipboardData &&
+      window.clipboardData.getData
+    ) {
+      // compatibility with older IE versions
+      text = window.clipboardData.getData("Text");
+    } else {
+      text = e.clipboardData.getData("text/plain");
+      html = e.clipboardData.getData("text/html");
+      file = e.clipboardData?.items?.[0];
+    }
+
     let delta = new Delta().retain(range.index).delete(range.length);
 
     const DOMPurifyOptions = this.getDOMPurifyOptions();
@@ -71,32 +85,59 @@ class QuillPasteSmart extends Clipboard {
       }
 
       // add hooks to accessible setttings
-      if (typeof this.hooks?.beforeSanitizeElements === 'function') {
-        DOMPurify.addHook('beforeSanitizeElements', this.hooks.beforeSanitizeElements);
+      if (typeof this.hooks?.beforeSanitizeElements === "function") {
+        DOMPurify.addHook(
+          "beforeSanitizeElements",
+          this.hooks.beforeSanitizeElements
+        );
       }
-      if (typeof this.hooks?.uponSanitizeElement === 'function') {
-        DOMPurify.addHook('uponSanitizeElement', this.hooks.uponSanitizeElement);
+      if (typeof this.hooks?.uponSanitizeElement === "function") {
+        DOMPurify.addHook(
+          "uponSanitizeElement",
+          this.hooks.uponSanitizeElement
+        );
       }
-      if (typeof this.hooks?.afterSanitizeElements === 'function') {
-        DOMPurify.addHook('afterSanitizeElements', this.hooks.afterSanitizeElements);
+      if (typeof this.hooks?.afterSanitizeElements === "function") {
+        DOMPurify.addHook(
+          "afterSanitizeElements",
+          this.hooks.afterSanitizeElements
+        );
       }
-      if (typeof this.hooks?.beforeSanitizeAttributes === 'function') {
-        DOMPurify.addHook('beforeSanitizeAttributes', this.hooks.beforeSanitizeAttributes);
+      if (typeof this.hooks?.beforeSanitizeAttributes === "function") {
+        DOMPurify.addHook(
+          "beforeSanitizeAttributes",
+          this.hooks.beforeSanitizeAttributes
+        );
       }
-      if (typeof this.hooks?.uponSanitizeAttribute === 'function') {
-        DOMPurify.addHook('uponSanitizeAttribute', this.hooks.uponSanitizeAttribute);
+      if (typeof this.hooks?.uponSanitizeAttribute === "function") {
+        DOMPurify.addHook(
+          "uponSanitizeAttribute",
+          this.hooks.uponSanitizeAttribute
+        );
       }
-      if (typeof this.hooks?.afterSanitizeAttributes === 'function') {
-        DOMPurify.addHook('afterSanitizeAttributes', this.hooks.afterSanitizeAttributes);
+      if (typeof this.hooks?.afterSanitizeAttributes === "function") {
+        DOMPurify.addHook(
+          "afterSanitizeAttributes",
+          this.hooks.afterSanitizeAttributes
+        );
       }
-      if (typeof this.hooks?.beforeSanitizeShadowDOM === 'function') {
-        DOMPurify.addHook('beforeSanitizeShadowDOM', this.hooks.beforeSanitizeShadowDOM);
+      if (typeof this.hooks?.beforeSanitizeShadowDOM === "function") {
+        DOMPurify.addHook(
+          "beforeSanitizeShadowDOM",
+          this.hooks.beforeSanitizeShadowDOM
+        );
       }
-      if (typeof this.hooks?.uponSanitizeShadowNode === 'function') {
-        DOMPurify.addHook('uponSanitizeShadowNode', this.hooks.uponSanitizeShadowNode);
+      if (typeof this.hooks?.uponSanitizeShadowNode === "function") {
+        DOMPurify.addHook(
+          "uponSanitizeShadowNode",
+          this.hooks.uponSanitizeShadowNode
+        );
       }
-      if (typeof this.hooks?.afterSanitizeShadowDOM === 'function') {
-        DOMPurify.addHook('afterSanitizeShadowDOM', this.hooks.afterSanitizeShadowDOM);
+      if (typeof this.hooks?.afterSanitizeShadowDOM === "function") {
+        DOMPurify.addHook(
+          "afterSanitizeShadowDOM",
+          this.hooks.afterSanitizeShadowDOM
+        );
       }
 
       if (plainText) {
@@ -111,31 +152,51 @@ class QuillPasteSmart extends Clipboard {
           html = this.convertTableContent(html);
         }
 
-        if (this.substituteBlockElements !== false) {
-          let substitution;
-          // html = DOMPurify.sanitize(html, { ...DOMPurifyOptions, ...{ RETURN_DOM: true, WHOLE_DOCUMENT: false } });
-          [html, substitution] = this.substitute(html, DOMPurifyOptions);
-          content = html.innerHTML;
-          if (this.removeConsecutiveSubstitutionTags) {
-            content = this.collapseConsecutiveSubstitutionTags(content, substitution);
-          }
-        } else {
-          content = DOMPurify.sanitize(html, DOMPurifyOptions);
-        }
-        delta = delta.concat(this.convert({ html: content }));
-      }
+      delta = delta.concat(this.convert(content));
+    } else if (
+      DOMPurifyOptions.ALLOWED_TAGS.includes("a") &&
+      this.isURL(text) &&
+      range.length > 0 &&
+      this.magicPasteLinks
+    ) {
+      content = this.quill.getText(range.index, range.length);
+      delta = delta.insert(content, {
+        link: text,
+      });
+    } else if (
+      DOMPurifyOptions.ALLOWED_TAGS.includes("img") &&
+      file &&
+      file.kind === "file" &&
+      file.type.match(/^image\//i)
+    ) {
+      const image = file.getAsFile();
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        this.quill.insertEmbed(range.index, "image", e.target.result);
+        // if required, manually update the selection after the file loads
+        if (!this.keepSelection) this.quill.setSelection(range.index + 1);
+      };
+      reader.readAsDataURL(image);
+    } else {
+      delta = delta.insert(content);
     }
 
     this.quill.updateContents(delta, Quill.sources.USER);
 
-    if (!plainText) {
-      // move cursor
-      delta = this.convert({ html: content });
-    }
-
-    if (this.keepSelection) this.quill.setSelection(range.index, delta.length(), Quill.sources.SILENT);
-    else this.quill.setSelection(range.index + delta.length(), Quill.sources.SILENT);
-    this.quill.scrollSelectionIntoView();
+    // move cursor
+    delta = this.convert(content);
+    if (this.keepSelection)
+      this.quill.setSelection(
+        range.index,
+        delta.length(),
+        Quill.sources.SILENT
+      );
+    else
+      this.quill.setSelection(
+        range.index + delta.length(),
+        Quill.sources.SILENT
+      );
+    this.quill.scrollIntoView();
     DOMPurify.removeAllHooks();
   }
 
@@ -225,76 +286,76 @@ class QuillPasteSmart extends Clipboard {
       let undefinedTags = false;
       if (tidy.ALLOWED_TAGS === undefined) {
         undefinedTags = true;
-        tidy.ALLOWED_TAGS = ['p', 'br', 'span'];
+        tidy.ALLOWED_TAGS = ["p", "br", "span"];
       }
 
       let undefinedAttr = false;
       if (tidy.ALLOWED_ATTR === undefined) {
         undefinedAttr = true;
-        tidy.ALLOWED_ATTR = ['class'];
+        tidy.ALLOWED_ATTR = ["class"];
       }
 
-      const toolbar = this.quill.getModule('toolbar');
+      const toolbar = this.quill.getModule("toolbar");
       toolbar?.controls?.forEach((control) => {
         switch (control[0]) {
-          case 'bold':
+          case "bold":
             if (undefinedTags) {
-              tidy.ALLOWED_TAGS.push('b');
-              tidy.ALLOWED_TAGS.push('strong');
+              tidy.ALLOWED_TAGS.push("b");
+              tidy.ALLOWED_TAGS.push("strong");
             }
             break;
 
-          case 'italic':
+          case "italic":
             if (undefinedTags) {
-              tidy.ALLOWED_TAGS.push('i');
-              tidy.ALLOWED_TAGS.push('em');
+              tidy.ALLOWED_TAGS.push("i");
+              tidy.ALLOWED_TAGS.push("em");
             }
             break;
 
-          case 'underline':
+          case "underline":
             if (undefinedTags) {
-              tidy.ALLOWED_TAGS.push('u');
+              tidy.ALLOWED_TAGS.push("u");
             }
             break;
 
-          case 'strike':
+          case "strike":
             if (undefinedTags) {
-              tidy.ALLOWED_TAGS.push('s');
+              tidy.ALLOWED_TAGS.push("s");
             }
             break;
 
-          case 'color':
-          case 'background':
+          case "color":
+          case "background":
             if (undefinedAttr) {
-              tidy.ALLOWED_ATTR.push('style');
+              tidy.ALLOWED_ATTR.push("style");
             }
             break;
 
-          case 'script':
+          case "script":
             if (undefinedTags) {
-              if (control[1].value === 'super') {
-                tidy.ALLOWED_TAGS.push('sup');
-              } else if (control[1].value === 'sub') {
-                tidy.ALLOWED_TAGS.push('sub');
+              if (control[1].value === "super") {
+                tidy.ALLOWED_TAGS.push("sup");
+              } else if (control[1].value === "sub") {
+                tidy.ALLOWED_TAGS.push("sub");
               }
             }
             break;
 
-          case 'header':
+          case "header":
             if (undefinedTags) {
               const detectAllowedHeadingTag = (value) => {
-                if (value === '1') {
-                  tidy.ALLOWED_TAGS.push('h1');
-                } else if (value === '2') {
-                  tidy.ALLOWED_TAGS.push('h2');
-                } else if (value === '3') {
-                  tidy.ALLOWED_TAGS.push('h3');
-                } else if (value === '4') {
-                  tidy.ALLOWED_TAGS.push('h4');
-                } else if (value === '5') {
-                  tidy.ALLOWED_TAGS.push('h5');
-                } else if (value === '6') {
-                  tidy.ALLOWED_TAGS.push('h6');
+                if (value === "1") {
+                  tidy.ALLOWED_TAGS.push("h1");
+                } else if (value === "2") {
+                  tidy.ALLOWED_TAGS.push("h2");
+                } else if (value === "3") {
+                  tidy.ALLOWED_TAGS.push("h3");
+                } else if (value === "4") {
+                  tidy.ALLOWED_TAGS.push("h4");
+                } else if (value === "5") {
+                  tidy.ALLOWED_TAGS.push("h5");
+                } else if (value === "6") {
+                  tidy.ALLOWED_TAGS.push("h6");
                 }
               };
 
@@ -307,64 +368,60 @@ class QuillPasteSmart extends Clipboard {
             }
             break;
 
-          case 'code-block':
+          case "code-block":
             if (undefinedTags) {
-              tidy.ALLOWED_TAGS.push('pre');
+              tidy.ALLOWED_TAGS.push("pre");
             }
             if (undefinedAttr) {
-              tidy.ALLOWED_ATTR.push('spellcheck');
+              tidy.ALLOWED_ATTR.push("spellcheck");
             }
             break;
 
-          case 'list':
+          case "list":
             if (undefinedTags) {
-              if (control[1].value === 'ordered') {
-                tidy.ALLOWED_TAGS.push('ol');
-              } else if (control[1].value === 'bullet') {
-                tidy.ALLOWED_TAGS.push('ul');
+              if (control[1].value === "ordered") {
+                tidy.ALLOWED_TAGS.push("ol");
+              } else if (control[1].value === "bullet") {
+                tidy.ALLOWED_TAGS.push("ul");
               }
-              tidy.ALLOWED_TAGS.push('li');
+              tidy.ALLOWED_TAGS.push("li");
             }
             break;
 
-          case 'link':
+          case "link":
             if (undefinedTags) {
-              tidy.ALLOWED_TAGS.push('a');
+              tidy.ALLOWED_TAGS.push("a");
             }
             if (undefinedAttr) {
-              tidy.ALLOWED_ATTR.push('href');
-              tidy.ALLOWED_ATTR.push('target');
-              tidy.ALLOWED_ATTR.push('rel');
+              tidy.ALLOWED_ATTR.push("href");
+              tidy.ALLOWED_ATTR.push("target");
+              tidy.ALLOWED_ATTR.push("rel");
             }
             break;
 
-          case 'image':
+          case "image":
             if (undefinedTags) {
-              tidy.ALLOWED_TAGS.push('img');
+              tidy.ALLOWED_TAGS.push("img");
             }
             if (undefinedAttr) {
-              tidy.ALLOWED_ATTR.push('src');
-              tidy.ALLOWED_ATTR.push('title');
-              tidy.ALLOWED_ATTR.push('alt');
-              tidy.ALLOWED_ATTR.push('height');
-              tidy.ALLOWED_ATTR.push('width');
+              tidy.ALLOWED_ATTR.push("src");
+              tidy.ALLOWED_ATTR.push("title");
+              tidy.ALLOWED_ATTR.push("alt");
             }
             break;
 
-          case 'video':
+          case "video":
             if (undefinedTags) {
-              tidy.ALLOWED_TAGS.push('iframe');
+              tidy.ALLOWED_TAGS.push("iframe");
             }
             if (undefinedAttr) {
-              tidy.ALLOWED_ATTR.push('frameborder');
-              tidy.ALLOWED_ATTR.push('allowfullscreen');
-              tidy.ALLOWED_ATTR.push('src');
-              tidy.ALLOWED_ATTR.push('height');
-              tidy.ALLOWED_ATTR.push('width');
+              tidy.ALLOWED_ATTR.push("frameborder");
+              tidy.ALLOWED_ATTR.push("allowfullscreen");
+              tidy.ALLOWED_ATTR.push("src");
             }
             break;
 
-          case 'blockquote':
+          case "blockquote":
             if (undefinedTags) {
               tidy.ALLOWED_TAGS.push(control[0]);
             }
@@ -402,34 +459,36 @@ class QuillPasteSmart extends Clipboard {
   substitute(html, DOMPurifyOptions) {
     let substitution;
 
-    const headings = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
+    const headings = ["h1", "h2", "h3", "h4", "h5", "h6"];
     const blockElements = [
-      'p',
-      'div',
-      'section',
-      'article',
-      'fieldset',
-      'address',
-      'aside',
-      'blockquote',
-      'canvas',
-      'dl',
-      'figcaption',
-      'figure',
-      'footer',
-      'form',
-      'header',
-      'main',
-      'nav',
-      'noscript',
-      'ol',
-      'pre',
-      'ul',
-      'video',
+      "p",
+      "div",
+      "section",
+      "article",
+      "fieldset",
+      "address",
+      "aside",
+      "blockquote",
+      "canvas",
+      "dl",
+      "figcaption",
+      "figure",
+      "footer",
+      "form",
+      "header",
+      "main",
+      "nav",
+      "noscript",
+      "ol",
+      "pre",
+      "table",
+      "tfoot",
+      "ul",
+      "video",
     ];
-    const newLineElements = ['li', 'dt', 'dd', 'hr'];
+    const newLineElements = ["li", "dt", "dd", "hr"];
 
-    DOMPurify.addHook('uponSanitizeElement', (node, data, config) => {
+    DOMPurify.addHook("uponSanitizeElement", (node, data, config) => {
       // check if current tag is a heading
       // - is it supported?
       // - no? - replace it with <p> and <b>
@@ -445,11 +504,16 @@ class QuillPasteSmart extends Clipboard {
       // find possible substitution
       let i = 0;
       while (!substitution && i < 3) {
-        if (DOMPurifyOptions.ALLOWED_TAGS.includes(blockElements[i])) substitution = blockElements[i];
+        if (DOMPurifyOptions.ALLOWED_TAGS.includes(blockElements[i]))
+          substitution = blockElements[i];
         ++i;
       }
 
-      if (substitution && node.tagName && !DOMPurifyOptions.ALLOWED_TAGS.includes(node.tagName.toLowerCase())) {
+      if (
+        substitution &&
+        node.tagName &&
+        !DOMPurifyOptions.ALLOWED_TAGS.includes(node.tagName.toLowerCase())
+      ) {
         const tagName = node.tagName.toLowerCase();
         if (headings.includes(tagName)) {
           node.innerHTML = `<${substitution}><b>${node.innerHTML}</b></${substitution}>`;
@@ -461,17 +525,77 @@ class QuillPasteSmart extends Clipboard {
       }
     });
 
-    html = DOMPurify.sanitize(html, { ...DOMPurifyOptions, ...{ RETURN_DOM: true, WHOLE_DOCUMENT: false } });
+    html = DOMPurify.sanitize(html, {
+      ...DOMPurifyOptions,
+      ...{ RETURN_DOM: true, WHOLE_DOCUMENT: false },
+    });
     DOMPurify.removeAllHooks();
 
-    return [html, substitution];
+    // fix quill bug #3333
+    // span content placed into the next tag
+
+    let depth = 0;
+    const walkTheDOM = (node, func) => {
+      func(node, depth);
+      // node = node.firstChild;
+      if (depth <= 1) node = node.firstChild;
+      else node = undefined;
+      while (node) {
+        ++depth;
+        walkTheDOM(node, func);
+        node = node.nextSibling;
+      }
+      --depth;
+    };
+
+    let block;
+    const fixedDom = document.createElement("body");
+    walkTheDOM(html, (node, depth) => {
+      if (depth === 1) {
+        if (
+          node.tagName &&
+          blockElements.includes(node.tagName.toLowerCase())
+        ) {
+          if (block) block = undefined;
+          const element = document.createElement(node.tagName.toLowerCase());
+          element.innerHTML = node.innerHTML;
+          fixedDom.appendChild(element);
+        } else {
+          if (block === undefined) {
+            block = document.createElement(substitution);
+            fixedDom.appendChild(block);
+          }
+
+          if (node.tagName) {
+            const element = document.createElement(node.tagName.toLowerCase());
+
+            const attributes = node.attributes;
+            if (attributes.length) {
+              Array.from(attributes).forEach((el) =>
+                element.setAttribute(el.nodeName, el.value)
+              );
+            }
+
+            if (node.innerHTML) element.innerHTML = node.innerHTML;
+            block.appendChild(element);
+          } else {
+            // plain text
+            const element = document.createTextNode(node.textContent);
+            block.appendChild(element);
+          }
+        }
+      }
+    });
+
+    return fixedDom;
   }
 
   isURL(str) {
-    const pattern = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?$/isu;
+    const pattern =
+      /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?$/isu;
     return !!pattern.test(str);
   }
 }
 
-Quill.register('modules/clipboard', QuillPasteSmart, true);
+Quill.register("modules/clipboard", QuillPasteSmart, true);
 export default QuillPasteSmart;
